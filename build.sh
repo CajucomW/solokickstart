f6d33c6f916c6aff99e4dc822cc7ba1cda67f25a
Wence Cajucom <cajucom.w@gmail.com>
2019-06-14 21:00:53 -0700

    created bash script for tech.html

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/tech.html templates/bottom.html > docs/tech.html
 xdg-open docs/index.html
