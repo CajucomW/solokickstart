536264d7ed5ea034d78e21e48ba9b1fd232f86e5
Wence Cajucom <cajucom.w@gmail.com>
2019-06-16 19:36:01 -0700

    added index.html to build.py

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
 top = open('templates/top.html').read()
 bottom = open('templates/bottom.html').read()
+
+content = open('index.html').read()
+index_html = top + content + bottom
+open('index_full.html', 'w+').write(index_html)
---
+++ build.py~
@@ -0,0 +1,2 @@
+top = open('templates/top.html').read()
+bottom = open('templates/bottom.html').read()
