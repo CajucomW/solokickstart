2ae042690509fbf64481a99aa60eb03b54fe6f89
Wence Cajucom <cajucom.w@gmail.com>
2019-06-18 22:10:25 -0700

    moved full.html files to docs/ but none of the links work

--- build.py
+++ build.py
@@ -3,16 +3,16 @@ bottom = open('templates/bottom.html').read()
 
 content = open('content/index.html').read()
 index_html = top + content + bottom
-open('index_full.html', 'w+').write(index_html)
+open('./docs/index_full.html', 'w+').write(index_html)
 
 content = open('content/tech.html').read()
 tech_html = top + content + bottom
-open('tech_full.html', 'w+').write(tech_html)
+open('./docs/tech_full.html', 'w+').write(tech_html)
 
 content = open('content/pta.html').read()
 pta_html = top + content + bottom
-open('pta_full.html', 'w+').write(pta_html)
+open('./docs/pta_full.html', 'w+').write(pta_html)
 
 content = open('content/jiujitsu.html').read()
 jiujitsu_html = top + content + bottom
-open('jiujitsu_full.html', 'w+').write(jiujitsu_html)
+open('./docs/jiujitsu_full.html', 'w+').write(jiujitsu_html)
