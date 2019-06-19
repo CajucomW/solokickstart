19d04d49f0013e84c6fe3422b2ae23372b35a621
Wence Cajucom <cajucom.w@gmail.com>
2019-06-19 09:36:32 -0700

    removed .py files from bash branch

--- build.py
+++
@@ -1,6 +0,0 @@
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
-
-content = open('index.html').read()
-index_html = top + content + bottom
-open('index_full.html', 'w+').write(index_html)
