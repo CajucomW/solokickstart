ddad03c319ce78708918128c22d980f453ae70ce
Wence Cajucom <cajucom.w@gmail.com>
2019-06-25 23:23:57 -0700

    Finally completed phase 2 along with tips from a classmate. Phases 1-3 work in conjunction, not separately, which I thought was the case initially. Phase 2 stores content files only, which then is string replaced as instructed in phase 3.

--- hw3.py
+++ hw3.py
@@ -22,52 +22,75 @@
 #if __name__ == "__main__":
 #    main()
     
-#pages = [   #TURN THIS INTO SOMETHING THAT PUTS OUT FULL HTML FILES
-#    {
+pages = [   #TURN THIS INTO SOMETHING THAT PUTS OUT FULL HTML FILES
+    {
+        'filename': 'content/tech.html',
+        'output': 'docs/tech.html',
+        'title': 'Tech',
+    },
+    {
+        'filename': 'content/pta.html',
+        'output': 'docs/pta.html',
+        'title': 'PTA/Healthcare',
+    },
+    {
+        'filename': 'content/jiujitsu.html',
+        'output': 'docs/jiujitsu.html',
+        'title': 'JiuJitsu',   
+    },
+    {
+        'filename': 'content/index.html',
+        'output': 'docs/index.html',
+        'title': 'About Me',
+    },
+    
+]
+template = open('templates/base.html').read()
+
+for html in pages:
+    filename = open(html['filename']).read()
+    combined_page = template.replace('{{content}}', filename)
+    open('docs/index.html', 'w+').write(combined_page)
+
+
+
+# So far this works.
+#topfile = pages[0]
+#indexfile = pages[1]
+#bottomfile = pages[2]
+#topcontent = open(topfile['filename']).read()
+#indexcontent = open(indexfile['filename']).read()
+#bottomcontent = open(bottomfile['filename']).read()
+#finalcontent = topcontent + indexcontent + bottomcontent
+#open('docs/index.html', 'w+').write(finalcontent)
+#{
 #        'filename': 'templates/top.html',
 #        'output': 'docs/index.html',
 #        'title': 'Top Document',
 #    },
-#    {
+#{
 #        'filename': 'templates/bottom.html',
 #        'output': 'docs/index.html',
 #        'title': 'Bottom Document',
 #    },
-#    {
-#        'filename': 'content/index.html',
-#        'output': 'docs/index.html',
-#        'title': 'About Me',
-#    },
-#    {
-#        'filename': 'content/tech.html',
-#        'output': 'docs/tech.html',
-#        'title': 'Tech',
-#    },
-#    {
-#        'filename': 'content/pta.html',
-#        'output': 'docs/pta.html',
-#        'title': 'PTA/Healthcare',
-#    },
-#    {
-#        'filename': 'content/jiujitsu.html',
-#        'output': 'docs/jiujitsu.html',
-#        'title': 'JiuJitsu',   
-#    },
-#]
-
-
-#for html in pages:
-#    files = html['filename']
-#    open(files).read
     
 
+# This code only writes the last entry in the list
+#    contents = open(html['filename']).read()
+#    open('test.html', 'w+').write(contents)
+    
+
+#    print('----')
+#    print(html['filename'])
+#    print(html['output'])
+#    print('----')
 
 
 # THIS ONE WORKS
-template = open('templates/base.html').read()
-index_content = open('content/index.html').read()
-finished_index_page = template.replace('{{content}}', index_content)
-open('docs/index.html', 'w+').write(finished_index_page)
+#template = open('templates/base.html').read()
+#index_content = open('content/index.html').read()
+#finished_index_page = template.replace('{{content}}', index_content)
+#open('docs/index.html', 'w+').write(finished_index_page)
 
 
 
