e29961262c3bcd9062a53970aca31c4597492961
Wence Cajucom <cajucom.w@gmail.com>
2019-06-25 20:48:29 -0700

    debug base.html and index.html missing </a>. Phase 3 seems to work. Still stuck with phase 2.

--- hw3.py
+++ hw3.py
@@ -1,26 +1,26 @@
-def main():
+#def main():
 
-    top = open('templates/top.html').read()
-    bottom = open('templates/bottom.html').read()
+#    top = open('templates/top.html').read()
+#    bottom = open('templates/bottom.html').read()
 
-    content = open('content/index.html').read()
-    index_html = top + content + bottom
-    open('docs/index.html', 'w+').write(index_html)
+#    content = open('content/index.html').read()
+#    index_html = top + content + bottom
+#    open('docs/index.html', 'w+').write(index_html)
 
-    content = open('content/tech.html').read()
-    tech_html = top + content + bottom
-    open('docs/tech.html', 'w+').write(tech_html)
+#    content = open('content/tech.html').read()
+#    tech_html = top + content + bottom
+#    open('docs/tech.html', 'w+').write(tech_html)
 
-    content = open('content/pta.html').read()
-    pta_html = top + content + bottom
-    open('docs/pta.html', 'w+').write(pta_html)
+#    content = open('content/pta.html').read()
+#    pta_html = top + content + bottom
+#    open('docs/pta.html', 'w+').write(pta_html)
 
-    content = open('content/jiujitsu.html').read()
-    jiujitsu_html = top + content + bottom
-    open('docs/jiujitsu.html', 'w+').write(jiujitsu_html)
+#    content = open('content/jiujitsu.html').read()
+#    jiujitsu_html = top + content + bottom
+#    open('docs/jiujitsu.html', 'w+').write(jiujitsu_html)
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
     
 #pages = [   #TURN THIS INTO SOMETHING THAT PUTS OUT FULL HTML FILES
 #    {
@@ -64,10 +64,10 @@ if __name__ == "__main__":
 
 
 # THIS ONE WORKS
-#template = open('templates/base.html').read()
-#index_content = open('content/index.html').read()
-#finished_index_page = template.replace('{{content}}', index_content)
-#open('docs/index.html', 'w+').write(finished_index_page)
+template = open('templates/base.html').read()
+index_content = open('content/index.html').read()
+finished_index_page = template.replace('{{content}}', index_content)
+open('docs/index.html', 'w+').write(finished_index_page)
 
 
 
