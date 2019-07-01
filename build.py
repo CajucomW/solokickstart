edff9003cef1bbf12afb3e924346907281b41435
Wence Cajucom <cajucom.w@gmail.com>
2019-07-01 07:52:10 -0700

    Still working on phase 1. Code only writing and putting together one html file. Not looping through the list for reasons unknown.

--- build.py
+++ build.py
@@ -1,23 +1,48 @@
-pages = []
+# Challenge 3 of Jinja Control Flow looks to have the answer to nav links being
+# selected when on that page.
+
+# -------Phase 1--------
+# Instead of using a fixed list, grab files from the content/ directory to
+# build the site 
+
+import glob
+import os
+
+all_html_files = glob.glob("content/*.html")
+output = glob.glob('docs/*.html')
+
+print(all_html_files)
+
+for html_path in all_html_files:
+    print(html_path)
+    file_name = os.path.basename(html_path)
+    print(file_name)
+
+#    name_only, extension = os.path.splitext(file_name)
+#    print(name_only)
+#    print(extension)
+
+    pages = []
+
+    pages.append({
+        "filename": html_path, 
+#        "title": "Index",
+        "output": "docs/" + file_name,
+        })
+    print(pages)
 
 template = open('templates/base.html').read()
 
-def main():
-    for content in pages:
-        html_file = open(content['filename']).read()
+def build():
+    for page in pages:
+        html_file = open(page['filename']).read()
         combined = template.replace('{{content}}', html_file)
-        open(content['output'], 'w+').write(combined)
+        open(page['output'], 'w+').write(combined)
 
-        # I can't get this to work.
-
-#            new_title = template.replace('{{title}}', content['title'])
-#            open(content['output'], 'w+').write(new_title)
-
-if __name__ == "__main__":
-    main()   
-
-
-
+build()
+      
+# if __name__ == "__main__":
+#     main()   
 
 
 
