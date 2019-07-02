ec8e21d98d86e3dbf57d113dd2622d3f7b25c707
Wence Cajucom <cajucom.w@gmail.com>
2019-07-02 08:28:15 -0700

    Phase 1 still working, althought unsuccessful at implementing string replace for dynamic <title> in each pages. Moving forward to prioritize phase 2.

--- build.py
+++ build.py
@@ -1,38 +1,32 @@
-# Challenge 3 of Jinja Control Flow looks to have the answer to nav links being
-# selected when on that page.
-
+# Challenge 3 of Jinja Control Flow looks to have the answer to nav links being selected when on that page.
 # -------Phase 1--------
-# Instead of using a fixed list, grab files from the content/ directory to
-# build the site 
+# Instead of using a fixed list, grab files from the content/ directory tobuild the site 
 
 import glob
 import os
 
 all_html_files = glob.glob("content/*.html")
 output = glob.glob('docs/*.html')
-
-print(all_html_files)
 template = open('templates/base.html').read()
 
-pages = []
-
 for html_path in all_html_files:
-    print(html_path)
     file_name = os.path.basename(html_path)
-    print(file_name)
+    name_only, extension = os.path.splitext(file_name)
 
+pages = []
+for page in pages:
+    
     pages.append({
         "filename": html_path, 
-        "title": "Index",
+        "title": name_only,
         "output": "docs/" + file_name,
-        })
-    print(pages)
+    })
 
-for page in pages: 
     html_file = open(page['filename']).read()
     combined = template.replace('{{content}}', html_file)
     open(page['output'], 'w+').write(combined)
-    
+
+
 # if __name__ == "__main__":
 #   main()   
 
