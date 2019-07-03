953f99535c5076896b0009c4a2a3dd3a9075a705
Wence Cajucom <cajucom.w@gmail.com>
2019-07-02 23:33:51 -0700

    Modules and virtualenv working. Mobile view of site buggy, likely due to deleted code. Investigate later.

--- build.py
+++ build.py
@@ -1,31 +1,61 @@
 # Challenge 3 of Jinja Control Flow looks to have the answer to nav links being selected when on that page.
 # -------Phase 1--------
-# Instead of using a fixed list, grab files from the content/ directory tobuild the site 
-
 import glob
 import os
+
 
 all_html_files = glob.glob("content/*.html")
 output = glob.glob('docs/*.html')
 template = open('templates/base.html').read()
 
+pages = []
+
 for html_path in all_html_files:
     file_name = os.path.basename(html_path)
     name_only, extension = os.path.splitext(file_name)
 
-pages = []
+    pages.append({
+    "filename": html_path, 
+    "title": name_only,
+    "output": "docs/" + file_name,
+})
+
 for page in pages:
     
-    pages.append({
-        "filename": html_path, 
-        "title": name_only,
-        "output": "docs/" + file_name,
-    })
-
-    tabs = page['title']
     html_file = open(page['filename']).read()
-    combined = template.replace('{{content}}', html_file,)
+    combined = template.replace('{{content}}', html_file)
+    combined = combined.replace('{{title}}', page['title'])
     open(page['output'], 'w+').write(combined)
+#
+# 
+#  -------Phase 2--------
+from jinja2 import Template
+#index_html = open("content/index.html").read()
+#template_html = open("templates/base.html").read()
+#template = Template(template_html)
+#print(template)
+#result = template.render(
+#    title="Homepage",
+#    content=index_html,
+#    )
+#print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- hw4.py
+++ hw4.py
@@ -2,63 +2,38 @@
 # -------Phase 1--------
 import glob
 import os
-
+# from jinja2 import Template
 
 all_html_files = glob.glob("content/*.html")
 output = glob.glob('docs/*.html')
 template = open('templates/base.html').read()
 
+pages = []
+
 for html_path in all_html_files:
     file_name = os.path.basename(html_path)
     name_only, extension = os.path.splitext(file_name)
 
-pages = []
-
-pages.append({
-"filename": html_path, 
-"title": name_only,
-"output": "docs/" + file_name,
+    pages.append({
+    "filename": html_path, 
+    "title": name_only,
+    "output": "docs/" + file_name,
 })
 
 for page in pages:
     
     html_file = open(page['filename']).read()
     combined = template.replace('{{content}}', html_file)
-    combined = combined.replace('{{title}}', page['title'])
     open(page['output'], 'w+').write(combined)
 #
 # 
 #  -------Phase 2--------
-from jinja2 import Template
-#index_html = open("content/index.html").read()
-#template_html = open("templates/base.html").read()
-#template = Template(template_html)
-#print(template)
-#result = template.render(
-#    title="Homepage",
-#    content=index_html,
-#    )
-#print(result)
 
+# index_html = open('html_path').read()
+# template_html = open('template').read()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# template = Template(template_html)
+# template.render(
+#         title="Homepage",
+#         content=index_html,
+# )
